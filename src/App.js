b4df16a1998cf44aad449eada6b372140dc42903
krupss19 <krupss17@gmail.com>
2026-06-09 18:45:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import AnnouncementBar from "./components/AnnouncementBar";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Categories from "./components/Categories";
+import Products from "./components/Products";
+import Testimonials from "./components/Testimonials";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <AnnouncementBar />
+      <Navbar />
+      <Hero />
+      <Categories />
+      <Products />
+      <Testimonials />
+      <Newsletter />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
